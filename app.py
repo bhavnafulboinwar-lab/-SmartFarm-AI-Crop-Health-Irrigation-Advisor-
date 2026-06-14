0d65c39e977cc88a4f0b1bfb3131cac51e1678a7
Bhavna Fulboinwar <bhavnafulboinwar@gmail.com>
2026-06-14 23:12:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import os
+from utils.predictor import predict_disease, irrigation_advisor
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    file = request.files["image"]
+
+    soil = float(request.form["soil"])
+    temp = float(request.form["temp"])
+    humidity = float(request.form["humidity"])
+
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    file.save(filepath)
+
+    disease = predict_disease(filepath)
+
+    irrigation = irrigation_advisor(
+        soil,
+        temp,
+        humidity
+    )
+
+    return render_template(
+        "result.html",
+        image=filepath,
+        disease=disease,
+        irrigation=irrigation
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ predictor.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+def predict_disease(image_path):
+
+    img = cv2.imread(image_path)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    green_pixels = cv2.inRange(
+        hsv,
+        (35,40,40),
+        (85,255,255)
+    )
+
+    green_ratio = np.sum(green_pixels > 0) / green_pixels.size
+
+    if green_ratio > 0.6:
+        return "Healthy Plant"
+    elif green_ratio > 0.3:
+        return "Leaf Spot Disease"
+    else:
+        return "Blight Disease"
+
+def irrigation_advisor(
+        soil,
+        temp,
+        humidity):
+
+    if soil < 30:
+        return "Water Immediately"
+
+    elif soil < 50:
+        return "Water within 12 Hours"
+
+    else:
+        return "No Irrigation Required"
