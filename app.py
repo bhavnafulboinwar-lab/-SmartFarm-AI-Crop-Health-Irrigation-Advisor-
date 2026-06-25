54cb425c949ac32f995391515f87208963407b3f
Bhavna Fulboinwar <bhavnafulboinwar@gmail.com>
2026-06-26 00:42:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.route("/")
 def home():
     return render_template("index.html")
